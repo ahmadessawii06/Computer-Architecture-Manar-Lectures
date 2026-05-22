61f2c27aab18a15f93ef42d59fbaca5fdfd2f24b
ahmadessawii06 <ahmadessawii06@gmail.com>
2026-05-22 22:05:28 +0300

    refactor: add new strcture for folders

--- scripts/script.js
+++ scripts/script.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const totalLecturesBadge = document.getElementById('total-lectures');
 
     // Fetch and render data
-    fetch('../data.json')
+    fetch('../data/data.json')
         .then(res => res.json())
         .then(data => {
             if (totalLecturesBadge) {
